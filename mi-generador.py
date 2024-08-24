2031ea5b1f87223e19961012ff629c86f57112b1
solmoon16 <mmoon@fi.uba.ar>
2024-08-23 22:28:39 -0300

    copy dic

--- mi-generador.py
+++ mi-generador.py
@@ -4,8 +4,17 @@ args = sys.argv
 file_name = args[1]
 client_num = int(args[2])
 
-with open(file_name, 'r') as f:
-    dic = yaml.safe_load(f)
+dic = {'name': 'tp0', 
+       'services': 
+            {'server': 
+                {'container_name': 'server', 'image': 'server:latest', 'entrypoint': 'python3 /main.py', 'environment': ['PYTHONUNBUFFERED=1', 'LOGGING_LEVEL=DEBUG'], 'networks': ['testing_net']}, 
+            'client1': 
+                {'container_name': 'client1', 'image': 'client:latest', 'entrypoint': '/client', 'environment': ['CLI_ID=1', 'CLI_LOG_LEVEL=DEBUG'], 'networks': ['testing_net'], 'depends_on': ['server']
+                }
+            }, 
+        'networks': 
+            {'testing_net': 
+                {'ipam': {'driver': 'default', 'config': [{'subnet': '172.25.125.0/24'}]}}}}
 
 services = dic['services']
 client = services['client1']
