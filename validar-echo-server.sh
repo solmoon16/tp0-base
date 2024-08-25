6c5fe8ed4e507e6c4d242118d39968d83668f3b9
solmoon16 <mmoon@fi.uba.ar>
2024-08-25 13:15:28 -0300

    change to receive message from cl

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
-message='holis'
-received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $message")
+received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $1")
 received=$(echo $received | tr -d '\n')
-if [[ "$received" == "$message" ]]; then
+if [[ "$received" == "$1" ]]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
