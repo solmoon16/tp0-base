54a4f63da5fdd68254cd3f604b146c642549209b
solmoon16 <mmoon@fi.uba.ar>
2024-08-25 23:34:17 -0300

    add client graceful shutdown

--- client/common/client.go
+++ client/common/client.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"syscall"
+	"os"
+	"os/signal"
 
 	"github.com/op/go-logging"
 )
@@ -23,13 +26,24 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
+	stop chan bool
+}
+
+func signalHandler(stop chan bool) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	stop <- true
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
+	stop := make(chan bool, 1)
+	go signalHandler(stop)
+
 	client := &Client{
 		config: config,
+		stop: stop,
 	}
 	return client
 }
@@ -50,40 +64,57 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
+func (c *Client) closeAll() {
+	log.Infof("action: received closing signal | info: closing socket | client_id: %v", c.config.ID)
+	if c.conn != nil {c.conn.Close()}
+}
+
+func (c *Client) handleConnection(msgID int) {
+	// Create the connection the server in every loop iteration. Send an
+	c.createClientSocket()
+
+	// TODO: Modify the send to avoid short-write
+	fmt.Fprintf(
+		c.conn,
+		"[CLIENT %v] Message N°%v\n",
+		c.config.ID,
+		msgID,
+	)
+	msg, err := bufio.NewReader(c.conn).ReadString('\n')
+	c.conn.Close()
+
+	if err != nil {
+		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+			c.config.ID,
+			err,
+		)
+		return
+	}
+
+	log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+		c.config.ID,
+		msg,
+	)
+
+	// Wait a time between sending one message and the next one
+	time.Sleep(c.config.LoopPeriod)
+}
+
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
+
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
+		select {
+		case stop := <-c.stop:
+			if stop {
+				c.closeAll()
+				return
+			}
+		default:
+			c.handleConnection(msgID)
 		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
 	}
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
