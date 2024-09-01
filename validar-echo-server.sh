3ef8e87f73d56f66cf6eda7e363981f71d8c9d29
solmoon16 <mmoon@fi.uba.ar>
2024-09-01 19:03:04 -0300

    change to not receive msg

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
+message="hola"
 received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $1")
 received=$(echo $received | tr -d '\n')
-if [[ "$received" == "$1" ]]; then
+if [ "$received" = $message ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
