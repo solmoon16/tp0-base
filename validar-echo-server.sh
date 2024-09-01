822c8f327f7a436ee230225b87489ff79cc674c6
solmoon16 <mmoon@fi.uba.ar>
2024-09-01 18:58:24 -0300

    remove usr from path

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $1")
 received=$(echo $received | tr -d '\n')
 if [[ "$received" == "$1" ]]; then
