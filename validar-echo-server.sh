aa8b77a26f7a92cd676d6f742b97f8098165670a
solmoon16 <mmoon@fi.uba.ar>
2024-09-01 19:54:26 -0300

    remove unnused

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,7 +2,7 @@
 message="hola"
 received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $message")
 received=$(echo $received | tr -d '\n')
-if [ "$received" = $message ]; then
+if [ $received = $message ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
