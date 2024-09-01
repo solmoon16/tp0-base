fed91b4f33d060c47dc693ebefd1a610171e1101
solmoon16 <mmoon@fi.uba.ar>
2024-09-01 20:01:57 -0300

    fix script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 message="hola"
-received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $message")
-echo $received
+received=$(docker run --rm --network=tp0_testing_net busybox sh -c "echo $message | nc server 12345")
 received=$(echo $received | tr -d '\n')
-if [ $received = $message ]; then
+if [ "$received" = $message ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
