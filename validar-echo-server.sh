1687ddceb3bfcf67c82dc2cb51b521a887a2237f
solmoon16 <mmoon@fi.uba.ar>
2024-09-01 19:57:24 -0300

    add debug print

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 message="hola"
 received=$(docker run --rm --network=tp0_testing_net busybox sh -c "nc server 12345 && echo $message")
+echo $received
 received=$(echo $received | tr -d '\n')
 if [ $received = $message ]; then
     echo "action: test_echo_server | result: success"
