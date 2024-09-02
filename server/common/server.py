1d4b0e5835f9b6aaf4ba548cf05dc4f44116b2ab
solmoon16 <mmoon@fi.uba.ar>
2024-09-02 20:49:39 -0300

    add pipes to communicate processes (not working)

--- server/common/server.py
+++ server/common/server.py
@@ -1,3 +1,6 @@
+from multiprocessing import Pipe, Process
+from multiprocessing.connection import wait
+import select
 import signal
 import socket
 import logging
@@ -7,7 +10,7 @@ from common.utils import Bet, has_won, load_bets, store_bets
 END_OF_BET=";"
 FIELD_SEPARATOR=","
 END_BATCH = "\n"
-AGENCIES_NUM = 5
+AGENCIES_NUM = 1
 DONE = "DONE:"
 
 class ServerSignalHandler:
@@ -26,7 +29,12 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._server_socket.setblocking(0)
+        readable, _, _ = select.select([self._server_socket], [], [self._server_socket], 1) 
+        self.readable = readable
         self.clients = {}
+        self.processes = []
+        self.pipes = []
 
     def run(self):
         """
@@ -39,12 +47,20 @@ class Server:
         
         while True and self._server_socket is not None:
             if self.clients_done():
+                logging.info("done")
+                for p in self.processes:
+                    p.join()
                 self.do_draw()
                 break
             client_socket = self.__accept_new_connection()
             if client_socket is None:
                 continue
-            self.__handle_client_connection(client_socket)
+            parent_conn, child_conn = Pipe(duplex=True)
+            p = Process(target=self.__handle_client_connection, args=(client_socket, child_conn))
+            self.processes.append(p)
+            p.start()
+            self.pipes.append(parent_conn)
+            child_conn.close()
         
             
     
@@ -55,8 +71,8 @@ class Server:
         Function blocks until a connection to a client is made.
         Then connection created is printed and returned
         """
-        # Connection arrived
-        logging.info('action: accept_connections | result: in_progress')
+        if self._server_socket is None:
+            return None
         try:
             c, addr = self._server_socket.accept()
             logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
@@ -65,16 +81,20 @@ class Server:
         else:
             return c
 
-    def __handle_client_connection(self, client_socket):
+    def __handle_client_connection(self, client_socket, pipe):
         """
         Read message from a specific client socket and closes the socket
 
         If a problem arises in the communication with the client, the
         client socket will also be closed
         """
+        
         old_msg = ""
         try:
-            while True and client_socket is not None:
+            while True:
+                if continue_connection(pipe) is False:
+                    client_socket.close()
+                    return
                 read = client_socket.recv(1024).decode('utf-8', 'ignore')
                 if not read:
                     break
@@ -83,7 +103,8 @@ class Server:
                 try:
                     sep = msg.index(DONE)
                     client_id = msg[sep+len(DONE):]
-                    self.clients.update({client_id: client_socket})
+                    pipe.send({client_id:client_socket})
+                    pipe.close()
                     return
                 except:
                     try:
@@ -96,7 +117,7 @@ class Server:
                     
         except OSError as e:
             logging.error(f"action: receive_message | result: fail | error: {e}")
-            
+
 
     def handle_message(self, msg: string, client_socket: socket):
         """
@@ -129,10 +150,32 @@ class Server:
     
     def close_all(self):
         self._server_socket = close_socket(self._server_socket, 'server')
+        for p in self.pipes:
+            try:
+                p.send(False)
+            except:
+                continue
+        for p in self.processes:
+            try:
+                p.join()
+            except:
+                continue
         for (agency, conn) in self.clients.items():
             close_socket(conn, agency)
     
     def clients_done(self) -> bool: 
+        for p in wait(self.pipes, 0.5):
+            try:
+                msg = p.recv()
+                logging.info(msg)
+                self.clients.update(msg)
+            except EOFError:
+                logging.error("pipe closed")
+                self.pipes.remove(p)
+                return False
+            except:
+                return False
+
         return len(self.clients.keys()) == AGENCIES_NUM
             
     def do_draw(self):
@@ -148,10 +191,14 @@ class Server:
         winsPerAgency = [0 for x in range(AGENCIES_NUM)]
         for win in winners:
             winsPerAgency[win.agency-1] += 1
-        for (agency, conn) in self.clients.items():
+        """ for (agency, conn) in self.clients.items():
             agency = int(agency)
             self.send_response(conn, winsPerAgency[agency-1])
-            close_socket(conn, f"client{agency}")
+            close_socket(conn, f"client{agency}") """
+        for agency, conn in self.clients.items():
+            agency = int(agency)
+            self.send_response(conn, winsPerAgency[agency-1])
+            self.close_all()
 
 
 def close_socket(sock: socket, name: string):
@@ -168,3 +215,7 @@ def handle_bet(betStr: string):
     
     return Bet(params[0], params[1], params[2], params[3], params[4], params[5])
     
+def continue_connection(pipe) -> bool:
+      if pipe.poll(0.5):
+          return pipe.recv
+      return True  
